8dfa0383c5dfadcc10230f0a6344c32d59afc4ef
Prakhar Tripathi <gurawliprakhar@gmail.com>
2026-05-26 03:09:10 +0530

    fix: correct electron entry point for development mode

--- index.js
+++ index.js
@@ -5,8 +5,17 @@
  * This file is specified in package.json as the main entry point.
  * It tells Electron where to find the main process file.
  * 
- * In development, this starts the Electron app.
+ * In development, this starts the Electron app from source.
  * In production, it loads the built main process.
  */
 
-module.exports = require('./dist/electron/main/index.js');
+const isDev = require('electron-is-dev');
+const path = require('path');
+
+if (isDev) {
+  // Development: load from source
+  module.exports = require('./electron/main/index.js');
+} else {
+  // Production: load from built dist folder
+  module.exports = require('./dist/electron/main/index.js');
+}
